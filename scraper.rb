a861a36b062205f7a4b6900b2144c52cdd8263c6
Henare Degan <henare.degan@gmail.com>
2014-12-13 18:53:46 +1100

    Don't download bills we already have. Add some debugging output

--- scraper.rb
+++ scraper.rb
@@ -5,6 +5,11 @@ require 'scraperwiki-morph'
 @base_url = 'http://www.aph.gov.au/Parliamentary_Business/Bills_Legislation/Bills_Search_Results/Result?bId='
 
 def get_bill(id)
+  if (!ScraperWikiMorph.select("id from data where id='#{id}'").empty? rescue false)
+    puts "Skipping already saved bill #{id}"
+    return
+  end
+
   url = @base_url + id
   page = @agent.get url
   bill_details = page.at('dl.specs').search(:dd).map { |e| e.inner_text.strip }
@@ -21,6 +26,7 @@ def get_bill(id)
     summary: bill_details[5],
   }
 
+  puts "Saving bill #{id}"
   ScraperWikiMorph.save_sqlite([:id], bill)
 end
 
